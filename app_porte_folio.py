253f527869d8b3337eee8d075c6d48aa527d4f2b
david-b59 <31815239+david-b59@users.noreply.github.com>
2025-01-27 22:06:47 +0100

    Update app_porte_folio.py
    suppression de seaborn et matplotlib car non utile

--- app_porte_folio.py
+++ app_porte_folio.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 from st_on_hover_tabs import on_hover_tabs
 from pathlib import Path
 import base64
